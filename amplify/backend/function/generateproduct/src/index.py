dc178f0f1c63014ab453093277c4e073bf2d6642
M <maxmnovick@gmail.com>
2022-10-15 07:37:10 -0400

    added file upload and download

--- amplify/backend/function/generateproduct/src/index.py
+++ amplify/backend/function/generateproduct/src/index.py
@@ -1,24 +1,113 @@
+# for basic handling of api
 import json
+# import sys
+# import logging
+
+# for added fcns
+import re
+
+#logging.basicConfig(level=logging.DEBUG)
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
 
 def generate_catalog(vendor_product_data):
     print("vendor_product_data: " + str(vendor_product_data))
 
 def handler(event, context):
-  print('received event:')
-  print(event)
+    print('received event:')
+    print(event)
+    # logger.info('event')
+    # logger.info(event)
 
-  # queryStringParameters = { param1: x, param2: y }
-  collection = event.get('queryStringParameters')['collection']
-  type = event.get('queryStringParameters')['type']
-  handle = collection + '-' + type
-  final_data = json.dumps(handle)  #json.dumps('Hello from your new Amplify Python lambda!')
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': final_data
-  }
+
+    # queryStringParameters = { param1: x, param2: y }
+    submit_type = event.get('queryStringParameters')['submit_type']
+    final_data = json.dumps('')
+
+    # if submit_type == 'form fill':
+
+    #     rows = event.get('queryStringParameters')['vendor_product_json']
+    #     final_data = json.dumps(rows)
+        
+        # collection = event.get('queryStringParameters')['collection']
+        # type = event.get('queryStringParameters')['type']
+    
+    
+    
+        # handle = collection + '-' + type
+
+
+
+        # final_data = json.dumps(handle)  #json.dumps('Hello from your new Amplify Python lambda!')
+    
+    #elif submit_type == 'upload file':
+        
+    # rows = event.get('queryStringParameters')['vendor_product_json']
+    # final_data = json.dumps(rows)
+
+
+    if submit_type == 'form':
+        collection = event.get('queryStringParameters')['collection']
+        type = event.get('queryStringParameters')['type']
+        handle = collection + '-' + type
+        final_data = json.dumps(handle)  #json.dumps('Hello from your new Amplify Python lambda!')
+    elif submit_type == 'file':
+        rows = json.loads(event.get('queryStringParameters')['vendor_product_json'])
+        col = event.get('queryStringParameters')['collection']
+        product_import_rows = []
+        import_row = {}
+
+        # find key names
+        # sku, collection, type
+        row = rows[0] # any row to get the keys
+        sku_key = 'sku'
+        col_key = 'collection'
+        type_key = 'type'
+        for key in row:
+            if re.search('Item#', key):
+                sku_key = key
+                print("sku_key: " + sku_key)
+            elif re.search('Collection', key):
+                col_key = key
+                print("col_key: " + col_key)
+            elif re.search('D E S C R I P T I O N', key):
+                type_key = key
+                print("type_key: " + type_key)
+
+        for row in rows:
+            print(row)
+            # from user input
+            # find sku key
+
+            sku = row[sku_key]
+            print("sku: " + sku)
+            collection = row[col_key]
+            print("collection: " + collection)
+            type = row[type_key]
+            print("type: " + type)
+            # generate variables
+            handle = collection + '-' + type
+            title = collection + ' ' + type
+            variant_sku = sku
+            #import_row = [handle, title, variant_sku]
+            # OR
+            import_row['handle'] = handle
+            import_row['title'] = title
+            import_row['variant_sku'] = variant_sku
+            product_import_rows.append(import_row)
+        final_data = json.dumps(product_import_rows) # rows, one for each product, ready for import
+        # collection = event.get('queryStringParameters')['collection']
+        # type = event.get('queryStringParameters')['type']
+        # handle = collection + '-' + type
+        # final_data = json.dumps(handle)  #json.dumps('Hello from your new Amplify Python lambda!')
+
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': final_data
+    }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { API, DataStore } from "aws-amplify";
+import { API, autoShowTooltip, DataStore } from "aws-amplify";
 import React, { useState } from "react";
 import { ProductDetails, ProductImport } from "../src/models";
 import { CatalogHeaders, CatalogRows, ImportHeaders, ImportRows, ProductDetailsForm } from "../src/ui-components";
@@ -13,6 +13,10 @@ export default function Home() {
 
   const [handle, setHandle] = useState("");
   const [title, setTitle] = useState("");
+
+  const [jsonRows, setJsonRows] = useState([{ text: 'Learn Hooks' }]);
+
+  const [importRows, setImportRows] = useState([])
 
   // const data_to_pass_in = {
   //   data_sent: 'data sent',
@@ -32,47 +36,193 @@ export default function Home() {
     }
   }
 
+  //excel import
+  const [tableData, setTableData] = useState([]);
+
+  const convertToJson = async (headers, data) => {
+    //debugger;
+    const rows = [];
+    data.forEach(async row => {
+      let rowData = {};
+      row.forEach(async (element, index) => {
+        rowData[headers[index]] = element;
+      });
+      console.log("rowData--->", rowData);
+      rows.push(rowData);
+    });
+    console.log('rows', rows);
+
+    setTableData(rows);
+    setJsonRows(rows);
+    //const str = "tableData: " + tableData
+    //console.log(tableData);
+
+    return(rows);
+  };
+
+  // const assignToCatalog = async (data_rows) => {
+  //   const catalog_rows = [];
+
+  // };
+
+  // at the end we want to have the useState variables assigned before processing
+  const assignVarsToModel = async (headers, data_rows) => {
+    // find sku column
+    // look for keywords like sku or item#
+    // find collection column with collection keyword
+    // find type column with description keyword
+    sku_column_name = 'sku'
+    for (h in headers){
+      console.log(h)
+    }
+    //should we pass this to python for processing to get the fields sorted? yes!
+
+  };
+
   //import excel
   const uploadToClient = async (e) => {
-    if (e.target.files && e.target.files[0]) {
-      const file = e.target.files[0];
-      console.log(file)
-      const reader = new FileReader()
-      reader.onload = (event) => {
-        const bstr = event.target.result
-        console.log(bstr)
-        const workBook = XLSX.read(bstr, { type: "binary" })
-        console.log(workBook)
-        const workSheetName = workBook.SheetNames[0]
-        console.log(workSheetName)
-        const workSheet = workBook.Sheets[workSheetName]
-        console.log(workSheet)
-        const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
-        console.log(fileData)
-      }
-      reader.readAsBinaryString(file)
+    try {
+      if (e.target.files && e.target.files[0]) {
 
-      // [ [ data, from, file, 1 ], [data, from, file, 2 ] ]
-      // or JSON {}
-      const init_vendor_data = {
-        queryStringParameters: {
-          vendor_product_data: tableData // vendor product data is raw data from vendor fed to generate catalog fcn
+        const file = e.target.files[0];
+        console.log(file);
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const bstr = event.target.result;
+          console.log(bstr);
+          const workBook = XLSX.read(bstr, { type: "binary" });
+          console.log(workBook);
+          const workSheetName = workBook.SheetNames[0];
+          console.log(workSheetName);
+          const workSheet = workBook.Sheets[workSheetName];
+          console.log(workSheet);
+          const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+          console.log(fileData);
+          const headers = fileData[0];
+          console.log(headers);
+          const heads = headers.map(head => ({ title: head, field: head }));
+          console.log(heads);
+          fileData.splice(0, 1);
+          console.log(fileData);
+          convertToJson(headers, fileData);
         }
-      }
+        reader.readAsBinaryString(file);
 
-      // once we have the data we pass it to processing
-      const all_final_item_info = await API.get('productgeneratorapi','/product', init_vendor_data);
+        // sort out the input files so we get the fields we need
+        // for example a vendor gives us extra fields with non standard names so we look for keywords and the result is the standardized catalog
+
+
+        // assign vars from excel to data model
+        // for each row in table
+        // setSku();
+        // setCollection();
+        // setType();
+        //assignVarsToModel(headers, jsonRows);
+        
+
+        // [ [ data, from, file, 1 ], [data, from, file, 2 ] ]
+        // or JSON {}
+        // send vars from data model to generate product
+        // OR
+        // send vars direct from excel to generate product
+        
+        // we only need to get back the vendor catalog if we want to display it
+        // but for now just return the product import to avoid the extra steps
+        // const vendor_catalog = await API.get('productgeneratorapi', '/product', init_vendor_data) // list of each row not including header row
+        // const myInit = {
+        //   queryStringParameters: {
+        //     submit_type: 'bulk', // so return a list
+        //     collection: vendor_catalog,
+        //     type: type
+        //   }
+        // };
+        // once we have the data we pass it to processing
+        //const all_final_item_info = await API.get('productgeneratorapi','/product', init_vendor_data);
+        
+        // const init_vendor_data = {
+        //   queryStringParameters: {
+        //     submit_type: 'file',
+        //     //vendor_product_data: tableData // vendor product data is raw data from vendor fed to generate catalog fcn
+        //     vendor_product_json: jsonRows
+        //   }
+        // };
+        // const vendor_product_import = await API.get('productgeneratorapi', '/product', init_vendor_data) // list of each row not including header row
+        // //const handle = await API.get('productgeneratorapi','/product', myInit);
+        // console.log({vendor_product_import});
+        
+        //handles = vendor_product_import['handles']
+        // save data for each product or can we instead display it directly and make xlsx file download?
+        // vendor_product_import.forEach(async product => {
+        //   await DataStore.save(
+        //     new ProductImport({
+        //       Handle: product['handle'],
+        //       Title: product['title'],
+        //       Variant_SKU: product['sku']
+        //     })
+        //   );
+        // });
+        
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const uploadToServer = async () => {
+    try {
+      console.log(jsonRows[0]['Collection Name']);
+      const myInit = {
+        queryStringParameters: {
+          submit_type: 'file',
+          collection: jsonRows[0]['Collection Name'],
+          type: 'type example',
+          vendor_product_json: JSON.stringify(jsonRows)
+        }
+      };
+      // get the output data ready for import
+      // we know the indexes in standard format so we save their values
+      // handle_idx = 0
+      // const all_final_item_info = await API.get('productgeneratorapi','/product', myInit);
+      // handle = all_final_item_info[handle_idx]
+      const rows = await API.get('productgeneratorapi','/product', myInit);
+      //const handle = 'handle'
+      console.log({ rows });
+      // gen worksheet and workbook
+      setImportRows(rows);
+      
+
+
+      // const data = {
+      //   Data1: [
+      //     { name: 'gfg1', category: 'gfg4' },
+      //     { name: 'gfg2', category: 'gfg5' },
+      //     { name: 'gfg3', category: 'gfg6' },
+      //   ],
+      //   // Worksheet named pokemons
+      //   Data2: [
+      //     { name: 'gfg1', category: 'gfg1' },
+      //     { name: 'gfg1', category: 'gfg1' },
+      //     { name: 'gfg1', category: 'gfg1' },
+      //   ],
+      // };
+      // setImportRows(data);
+
+      document.getElementById('import_json').innerHTML = JSON.stringify(rows);
+      
+      //console.log("Post saved successfully!");
+    } catch(e) {
+      console.log(e);
     }
   };
 
 
 
   // save manually
-
   const saveProduct = async () => {
     try {
       const myInit = {
         queryStringParameters: {
+          submit_type: 'form',
           collection: collection,
           type: type
         }
@@ -110,6 +260,31 @@ export default function Home() {
     }
   }
 
+
+
+  const xport = async () => {
+    try {
+      // go from json to workbook
+      // can also go from table to workbook with table_to_book(table)
+      const import_worksheet = XLSX.utils.json_to_sheet(importRows);
+      const import_workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(import_workbook, import_worksheet, "Products");
+
+      //fix headers
+      XLSX.utils.sheet_add_aoa(import_worksheet, [["Handle", "Title", "Variant SKU"]], { origin: "A1" });
+
+      // calc column width
+      // const max_width = importRows.reduce((w, r) => Math.max(w, r.name.length), 10);
+      // import_worksheet["!cols"] = [ { wch: max_width } ];
+
+      XLSX.writeFile(import_workbook, "Product-Import.xlsx");
+    
+    } catch(err) {
+      console.log(err);
+    }
+      
+  };
+
   const addProductOverrides = {
     "TextField34462690": {
       onChange: (event) => {
@@ -140,6 +315,17 @@ export default function Home() {
             id="product_data" name="product_data"
             accept=".xlsx,.xls,.tsv,.csv"
             onChange={uploadToClient}/> 
+      <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={uploadToServer}
+        >
+          Send to server
+        </button>
+      {/* <a href="Product-Import.xlsx" download>Download Product Import</a> */}
+      <button onClick={xport}>Download Product Import</button>
+
+      <pre id="import_json"></pre>
 
       <ProductDetailsForm overrides={addProductOverrides}/>
       <div className="row">
